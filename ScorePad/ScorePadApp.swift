cbc602a114ed7c44b2bd6dd77ce6687c147749bf
R.J. LaCount <rj@turnstyle.studio>
2026-06-29 15:45:33 -0700

    Initial Commit

---
+++ ScorePad/ScorePadApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScorePadApp.swift
+//  ScorePad
+//
+//  Created by R.J. LaCount on 6/29/26.
+//
+
+import SwiftUI
+
+@main
+struct ScorePadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
